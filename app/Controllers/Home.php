2f9de055f3097c3df87ced02d64b01b5c198867e
Fajar <muhammad.fajar1991@gmail.com>
2021-10-29 03:19:38 +0700

    update base storymap

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,22 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        return view('landing_page');
-    }
+	// public function index()
+	// {
+	//     return view('landing_page');
+	// }
+
+	public function index()
+	{
+		return view('link_dusun');
+	}
+
+	public function maps()
+	{
+		$data = [
+			'title' => 'map',
+		];
+
+		return  view('storymap', $data);
+	}
 }
---
+++ public/storymaps/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1StN1Xs96Nl-vboSF8WIOpzzp1BVYSahHDmfpPlUAxz4/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyCC7L-Kkytx8Qewi0qDP2IVqxY9_O5ltWk';
